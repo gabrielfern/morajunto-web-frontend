3cfda1b3f8584e75b62ad34923a04cb2aae6e429
Gabriel Fernandes <gabrielfernnd@gmail.com>
2018-05-21 00:12:14 -0300

    using loaders to load dependency of materialize-css, so no need to keep the min files at static

--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import 'materialize-css/dist/js/materialize.min.js'
+import 'materialize-css/dist/css/materialize.min.css'
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: {App},
+  components: { App },
   template: '<App/>'
 })
