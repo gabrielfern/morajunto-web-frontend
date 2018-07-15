dcd3be111ab30ef48010a4dc80675f4a24ea6cf3
Gabriel Fernandes <gabrielfernnd@gmail.com>
2018-07-15 00:36:54 -0300

    modo avancado de edicao de informacoes para atualizar a senha, drop view antiga para edicao de senha

--- src/components/User.vue
+++ src/components/User.vue
@@ -22,12 +22,15 @@
     </div>
     <div class="row">
       <div class="input-field col s4">
-        <input type="text" v-model="user.username">
+        <input id="editmode-username" type="text" v-model="user.username">
         <label>Nome de usuário</label>
       </div>
       <div class="input-field col s4">
-        <input type="password" v-model="user.password">
-        <label>Senha</label>
+        <input id="editmode-password" type="password" v-model="user.password">
+        <label id="editmode-password-label">Senha</label>
+      </div>
+      <div class="input-field col s1" v-if="editMode">
+        <button class="btn" @click="switchAdvancedEditMode"><i class="material-icons">security</i></button>
       </div>
     </div>
     <div class="row" v-if="editMode">
@@ -41,7 +44,6 @@
     </div>
     <div class="row" v-if="!editMode">
       <div class="col s9"><a href="#" @click="goToCadastrarAnuncio"><button class="waves-effect waves-light btn">Criar anúncio</button></a>
-        <a href="#" @click="goToUpdateUser"><button class="btn waves-effect waves-light blue lighten-2">Editar senha</button></a>
         <a href="#" @click="goToAnuncios"><button class="btn waves-effect waves-light blue lighten-2">Anúncios</button></a>
       </div>
       <div class="s12 m4 l2">
@@ -62,7 +64,8 @@ export default {
   data () {
     return {
       user: loginControl.loginData.user,
-      editMode: false
+      editMode: false,
+      advancedEditMode: false
     }
   },
 
@@ -103,12 +106,33 @@ export default {
       this.user = Object.assign({}, loginControl.loginData.user)
       this.editMode = true
       $('input').prop('disabled', false)
+      $('#editmode-username').prop('disabled', true)
+      $('#editmode-password').prop('disabled', true)
     },
 
     submitEditMode () {
-      loginControl.loginData.user = this.user
-      ajax.updateUser()
-      this.getOutEditMode()
+      if (!this.user.password) {
+        // eslint-disable-next-line
+        modalErroruserupdate.open()
+        $('#modal-erroruserupdate-close').focus()
+      } else {
+        loginControl.loginData.user = this.user
+        ajax.updateUser()
+        this.getOutEditMode()
+      }
+    },
+
+    switchAdvancedEditMode () {
+      this.advancedEditMode = !this.advancedEditMode
+      if (this.advancedEditMode) {
+        $('#editmode-password').prop('disabled', false)
+        this.user.password = ''
+        $('#editmode-password').focus()
+      } else {
+        $('#editmode-password').prop('disabled', true)
+        this.user.password = loginControl.loginData.user.password
+        $('#editmode-password-label').addClass('active')
+      }
     }
   },
 
