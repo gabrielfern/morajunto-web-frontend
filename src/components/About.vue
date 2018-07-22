05392b52503cc2b025cd2f5a9158454c39f22a21
Gabriel Fernandes <gabrielfernnd@gmail.com>
2018-07-22 13:03:50 -0300

    atualizado o sobre

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
     <h5 style="color: teal">Sobre o aplicativo</h5>
-    MoraJunto app, um sistema desenvolvido para o projeto de Engenharia de Software
+    MoraJunto app, um sistema desenvolvido para o projeto de Engenharia de Software.
+    <br>
+    O sistema MoraJunto tem como principal objetivo ajudar estudantes e profissionais oriundos de cidades diferentes da do seu local futuro de estudo/trabalho a encontrarem uma moradia digna, justa e confortável a um preço acessível, podendo fazer isto a partir do seu local de origem via internet, sendo assim evitando o risco de procurar um local somente quando vir a este novo local.
+    <br>
     <br>
     <a href="https://github.com/gabrielfern/morajunto-web-frontend" target="_blank">Código fonte</a>
   </div>
